2ae3d5f1bccb62fa3a07cb1933d36d450e11b20c
Rama <ramashankar941k@gmail.com>
2023-12-06 23:15:16 +0530

    uploading post and show in  profile page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require("./users");
+const postModel = require("./posts");
 const passport = require('passport');
+const upload = require("./multer");
 
 const localStrategy = require('passport-local');
 passport.use(new localStrategy(userModel.authenticate()));
@@ -11,13 +13,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login',{error:req.flash('error')});
 });
 router.get('/feed', function(req, res, next) {
   res.render('feed');
 });
-router.get('/profile',isLoggedIn, function(req, res, next) {
-  res.render('profile');
+
+router.post('/upload',upload.single('file') ,async function(req, res, next) {
+  if(!req.file){
+    return res.status(404).send("No file were given");
+  }
+ const user = await userModel.findOne({username :req.session.passport.user});
+  const post = await postModel.create({
+    image: req.file.filename,
+    imageText: req.body.filecaption,
+    user: user._id
+  })
+  user.posts.push(post._id);
+  await user.save()
+  res.redirect('/profile')
+});
+router.get('/profile',isLoggedIn,async function(req, res, next) {
+  let user = await userModel.findOne({
+    username : req.session.passport.user 
+  })
+  .populate('posts');
+  res.render('profile',{user});
 });
 router.post('/register', function(req, res) {
   const { username, email, fullname } = req.body;
@@ -33,14 +54,15 @@ router.post('/register', function(req, res) {
   
 router.post('/login',passport.authenticate("local",{
     successRedirect : "/profile",
-    failureRedirect : "/login"
+    failureRedirect : "/login",
+    failureFlash:true
   }), function(req, res, next) {
   });
 
 router.get('/logout', function(req, res, next){
     req.logout(function(err) {
       if (err) { return next(err); }
-      res.redirect('/');
+      res.redirect('/login');
     })
   }); 
 
